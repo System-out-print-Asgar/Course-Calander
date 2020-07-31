384a5d094378e8182c45e204199932da709d2980
Gar <aymanasgar@gmail.com>
2020-07-31 18:41:55 -0400

    Update CourseObject.java

--- CourseObject.java
+++ CourseObject.java
@@ -1,1 +1,22 @@
 //This will hold the methods of the course objects and what will work on them
+
+public class Course {
+
+    //private variables of course object
+    String CourseCode;
+    String CourseName;
+    String CourseDescription;
+    String CourseTime;
+    Boolean RequiredCourse;
+    Boolean Group1Course;
+    Boolean TableALibCourse;
+    Boolean OpenElecCourse;
+    Boolean ProfRelateCourse;
+    Boolean TableBLibCourse;
+
+
+
+
+
+
+}
