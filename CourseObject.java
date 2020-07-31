eb6ec9058dbe5c116f990dbffb745a86bf78c25b
Gar <aymanasgar@gmail.com>
2020-07-31 18:50:27 -0400

    Update CourseObject.java

--- CourseObject.java
+++ CourseObject.java
@@ -1,19 +1,34 @@
+import java.security.PublicKey;
+
 //This will hold the methods of the course objects and what will work on them
 
-public class Course {
-
+public class CourseObject {
     //private variables of course object
-    String CourseCode;
-    String CourseName;
-    String CourseDescription;
-    String CourseTime;
-    Boolean RequiredCourse;
-    Boolean Group1Course;
-    Boolean TableALibCourse;
-    Boolean OpenElecCourse;
-    Boolean ProfRelateCourse;
-    Boolean TableBLibCourse;
+    private String CourseCode;
+    private String CourseName;
+    private String CourseDescription;
+    private String CourseTime;
+    private Boolean RequiredCourse;
+    private Boolean Group1Course;
+    private Boolean TableALibCourse;
+    private Boolean OpenElecCourse;
+    private Boolean ProfRelateCourse;
+    private Boolean TableBLibCourse;
 
+
+    //constructor method for course objects
+    public CourseObject(String Code, String Name, String Description, String Time, Boolean Required, Boolean G1, Boolean TAlib, Boolean TBlib, Boolean OpenEl, Boolean ProfRele){
+        CourseCode = Code;
+        CourseName = Name;
+        CourseDescription = Description;
+        CourseTime = Time;
+        RequiredCourse = Required;
+        Group1Course = G1;
+        TableALibCourse = TAlib;
+        TableBLibCourse = TBlib;
+        OpenElecCourse = OpenEl;
+        ProfRelateCourse = ProfRele;
+    }
 
 
 
