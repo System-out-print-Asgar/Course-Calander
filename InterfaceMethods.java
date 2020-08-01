8be769512f5b11b38fee6657f1801d507aec193f
Gar <aymanasgar@gmail.com>
2020-08-01 08:48:04 -0400

    Update InterfaceMethods.java

--- InterfaceMethods.java
+++ InterfaceMethods.java
@@ -1,1 +1,24 @@
-//This will hold all the methods that the Interface will be calling
+//this holds the methods in the user interface
+import java.io.File;
+import java.io.FileNotFoundException;;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Scanner;
+
+
+public class InterfaceMethods{
+    
+    //map that holds all the course 
+    public TreeMap <String,CourseObject> CourseList = new TreeMap <String,CourseObject>();
+
+
+    //method that prints out all the courses currently in database
+    public void DisplayAllCourse(){
+
+        for (String key : CourseList.keySet()){
+            System.out.println(CourseList.get(key).getInfo());
+        }
+    }
+
+
+}
