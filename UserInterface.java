f1ce1152a93b373ec90dc28a620ba6e976a2d0fa
Gar <aymanasgar@gmail.com>
2020-08-01 09:19:08 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,3 +1,28 @@
 //This is the user interface for selecting what actions to run
 
+import java.util.Scanner;
+
+public class UserInterface{
+
+    public static void main(String[][] args) throws Exception {
+
+        //creates a instaance of the mthod file
+        InterfaceMethods iMethods = new InterfaceMethods();
+
+        //create the scanner 
+        Scanner userScanner = new Scanner(System.in);
+
+        
+
+
+
+
+
+
+
+
+
+    }
+}
+
 
