7e1592e29ea57d1e5dfdd58fb87ad3596acba38c
Gar <aymanasgar@gmail.com>
2020-08-01 09:19:04 -0400

    Update InterfaceMethods.java

--- InterfaceMethods.java
+++ InterfaceMethods.java
@@ -11,6 +11,19 @@ public class InterfaceMethods{
     //map that holds all the course 
     public TreeMap <String,CourseObject> CourseList = new TreeMap <String,CourseObject>();
 
+    //method to display commands
+    public void StartUpCommands(){
+        System.out.println("Welcome to course calander V1.0");
+        System.out.println("To start the program please begin with entering the number of a command below:");
+        System.out.println("1-Display all current courses in the database");
+        System.out.println("2-Add a new course to the database");
+        System.out.println("3-Look up a specfic course in data base using ID");
+        System.out.println("4-Look up all courses within a specfic time frame");
+        System.out.println("5-Look up all courses within a specfic program");
+        System.out.println("(Enter any other key to move to access the second menu)");
+
+    }
+
 
     //method that prints out all the courses currently in database
     public void DisplayAllCourse(){
@@ -36,6 +49,7 @@ public class InterfaceMethods{
             }
         }
     }
+    
     
 
 
