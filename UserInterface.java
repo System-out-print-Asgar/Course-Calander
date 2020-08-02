8e540760c23a72e7a2d95c238b0c95bbd517aca5
Gar <aymanasgar@gmail.com>
2020-08-02 10:45:21 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -4,16 +4,38 @@ import java.util.Scanner;
 
 public class UserInterface{
 
-    public static void main(String[][] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
         //creates a instaance of the mthod file
         InterfaceMethods iMethods = new InterfaceMethods();
+
+        //print start up method
+        iMethods.StartUpCommands();
 
         //create the scanner 
         Scanner scanner = new Scanner(System.in);
         System.out.print(">");
 
-        while(scanner.nextLine()){
+        //scanner that checks if there is a extra line present and will procced with the code 
+        while(scanner.hasNextLine()){
+            String input = scanner.nextLine();
+            if(input == null || input.equals("")) continue;
+
+            //create a second scanner that will manage user input
+            Scanner Userinput = new Scanner(input);
+            String UserCommand =  Userinput.next();
+
+            
+
+            //code will contiue if there is no input
+            if (UserCommand == null || UserCommand.equals("")) continue;
+
+            //method to display all courses in database
+            else if (UserCommand.equals("Display")){
+                System.out.println("We will display all courses now!");
+            }
+
+
 
             
         }
